df01d92eff01118a173fc4ad8ef560e038fbd727
twofiive <go.boyy87@gmail.com>
2024-12-17 11:52:56 +0100

    Test function

---
+++ app/app/calc.py
@@ -0,0 +1,29 @@
+"""
+    Calculator functions
+"""
+
+
+def add(x, y):
+    """_summary_
+
+    Args:
+        x (_type_): _description_
+        y (_type_): _description_
+
+    Returns:
+        x + y: Addition
+    """
+    return x + y
+
+
+def subtract(x, y):
+    """_summary_
+
+    Args:
+        x (_type_): _description_
+        y (_type_): _description_
+
+    Returns:
+        x - y: Subtract
+    """
+    return x - y
